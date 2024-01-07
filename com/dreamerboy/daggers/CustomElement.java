54671b8e1d536e6d32f8a88e5a69ec26bd8f047e
DreamerBoy <45047570+DreamerrBoy@users.noreply.github.com>
2024-01-07 09:23:35 +0300

    Update CustomElement.java

--- com/dreamerboy/daggers/CustomElement.java
+++ com/dreamerboy/daggers/CustomElement.java
@@ -1,17 +1,24 @@
 package com.dreamerboy.daggers;
 
-import org.bukkit.ChatColor;
-
 import com.projectkorra.projectkorra.Element;
 import com.projectkorra.projectkorra.Element.ElementType;
 import com.projectkorra.projectkorra.Element.SubElement;
+import com.projectkorra.projectkorra.configuration.ConfigManager;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class CustomElement {
 
 	public static final SubElement DAGGERS = new SubElement("Daggers", Element.CHI, ElementType.NO_SUFFIX, Daggers.plugin) {
-		public ChatColor getColor() {
-			return ChatColor.YELLOW;
-		}
+	
+		@Override
+		public net.md_5.bungee.api.ChatColor getColor() {
+			final String color = ConfigManager.languageConfig.get().getString("Chat.Colors.Daggers");
+			if(color == null)
+				return ChatColor.YELLOW;
+			return ChatColor.of(color);
+		};
+		
 	};
 	
 }
